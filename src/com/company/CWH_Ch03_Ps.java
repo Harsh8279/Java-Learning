fb2144218c63baf10497a6799258a43ea071ea30
Harsh8279 <pharsh.spt@gmail.com>
2021-10-10 10:49:10 +0530

    question 2 solved

--- src/com/company/CWH_Ch03_Ps.java
+++ src/com/company/CWH_Ch03_Ps.java
@@ -9,7 +9,7 @@ public class CWH_Ch03_Ps {
         *
         *   Q-1  convert a string into lowercase
         *
-        * */
+
 
         System.out.println("Enter The String : ");
         String str = sc.nextLine();
@@ -17,7 +17,19 @@ public class CWH_Ch03_Ps {
         str = str.toLowerCase();
 
         System.out.println(str);
+         */
 
+        /*
+        *
+        *  Q-2 Write a Java Program to replace space with Underscores
+        *
+        * */
 
+        System.out.println("ENter The String : ");
+        String str = sc.nextLine();
+
+        str = str.replace(" ","_");
+
+        System.out.println(str);
     }
 }
