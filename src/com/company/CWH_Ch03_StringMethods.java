b4e827f20c3cda623c3b468a3427c61316040486
Harsh8279 <pharsh.spt@gmail.com>
2021-10-10 10:41:19 +0530

    string methods completed

--- src/com/company/CWH_Ch03_StringMethods.java
+++ src/com/company/CWH_Ch03_StringMethods.java
@@ -24,6 +24,28 @@ public class CWH_Ch03_StringMethods {
         System.out.println(str.replace("rsh","aa"));
         System.out.println(str.replace("r","hi"));
 
+        System.out.println(str.startsWith("H"));
+        System.out.println(str.startsWith("Par"));
+        System.out.println(str.endsWith("ah"));
+        System.out.println(str.endsWith("rsh"));
+
+        System.out.println(str.charAt(2));  // this is used to get char at specific index
+        System.out.println(str.indexOf("h"));   // this is used to get index of specific char (first occurrence only)
+
+        System.out.println(str.indexOf("ar"));
+        System.out.println(str.indexOf("a",3));
+        System.out.println(str.indexOf("r",1));
+
+        str = "Harry";
+
+        System.out.println(str.lastIndexOf("r"));
+        System.out.println(str.lastIndexOf("r",4));
+
+        System.out.println(str.equals("Harry"));
+        System.out.println(str.equals("Harsh"));
+        System.out.println(str.equalsIgnoreCase("harry"));
+
+        System.out.println("I \'m Harsh");
 
 
     }
